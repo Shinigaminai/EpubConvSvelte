da35c4b9669ca2d4ef905967b409eefaa1794388
Shinigaminai <24736346+Shinigaminai@users.noreply.github.com>
2025-01-01 18:44:25 +0100

    fix: hover help box

---
+++ src/lib/help.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import HelpIcon from '~icons/material-symbols/help-outline-rounded';
+
+	let { children } = $props();
+</script>
+
+<div class="nogrow containerHelp relative">
+	<HelpIcon class="opacity-30 hover:opacity-100"></HelpIcon>
+	<div class="help hidden">{@render children()}</div>
+</div>
+
+<style>
+	.containerHelp:hover > .help {
+		display: block;
+	}
+	.help {
+		display: hidden;
+		position: absolute;
+		padding: 1.5mm;
+		background-color: rgba(53, 38, 7, 0.6);
+		border-radius: 1.5mm;
+		backdrop-filter: blur(1.5mm);
+	}
+</style>
